367fefbfd8a34a4c427392779f16ef1f78def3fc
Gabriel Bonaretti <114812017+GabrielBonaretti@users.noreply.github.com>
2023-04-20 14:19:30 -0300

    Create Exercise5.java

---
+++ Exercises_List_6 (Real Life Data Structure)/Exercise5.java
@@ -0,0 +1,154 @@
+//  Classificações de filmes: construa um programa que usa um mapa para armazenar as classificações de filmes fornecidas
+//  pelos usuários. Implemente operações como adicionar novas classificações de filmes, atualizar classificações de filmes existentes, calcular classificações médias e exibir os filmes com melhor classificação.
+
+package Example.Exercises;
+
+import java.util.*;
+
+public class Exercise5List6 {
+    public static void main(String[] args) {
+        Map<String, Double> map = new HashMap<String, Double>();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            menu();
+
+            int choice = choice(sc);
+
+            if (choice == 1) {
+                add(sc, map);
+            } else if (choice == 2) {
+                remove(sc, map);
+            } else if (choice == 3) {
+                updateRating(sc, map);
+            } else if (choice == 4) {
+                retrieve(sc, map);
+            } else if (choice == 5) {
+                showList(map);
+            }else {
+                System.out.println("Enter one the options.");
+            }
+        }
+    }
+
+    static void menu(){
+        System.out.println("---------------------------------");
+        System.out.println("Movie classification:");
+        System.out.println("[1] Add movie");
+        System.out.println("[2] Remove movies");
+        System.out.println("[3] Updating movies");
+        System.out.println("[4] Searching for products");
+        System.out.println("[5] Show all movies");
+    }
+
+    static int choice(Scanner sc) {
+        int choice;
+
+        while (true) {
+            try {
+                System.out.print("Enter the option you want to perform: ");
+                choice = sc.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("You can only enter number.");
+            }
+            sc.nextLine();
+        }
+        sc.nextLine();
+
+        return choice;
+    }
+
+    static void add(Scanner sc, Map<String, Double> map){
+        String nameMovie;
+        System.out.println("---------------------------------");
+        System.out.print("Enter movie name: ");
+        nameMovie = sc.nextLine().toLowerCase();
+
+        Double rateMovie;
+
+        while (true){
+            try {
+                System.out.println("---------------------------------");
+                System.out.print("Enter movie rating: ");
+                rateMovie = sc.nextDouble();
+
+                if (rateMovie >= 0 || rateMovie <= 5) {
+                    break;
+                } else {
+                    System.out.println("The rating needs to be beetween 0 and 5 stars");
+                }
+            }catch (InputMismatchException e){
+                System.out.println("You can only enter number.");
+            }
+        }
+        map.put(nameMovie, rateMovie);
+    }
+
+    static void remove(Scanner sc, Map<String, Double> map){
+        String productName;
+
+        System.out.println("---------------------------------");
+        System.out.print("Enter the name of the movie you want to remove: ");
+        productName = sc.nextLine().toLowerCase();
+
+        if (map.containsKey(productName)) {
+            System.out.printf("The movie %s has been removed. \n", productName);
+            map.remove(productName);
+        } else {
+            System.out.printf("The movie %s is not in inventory.", productName);
+        }
+    }
+
+    static void updateRating(Scanner sc, Map<String, Double> map){
+        String nameMovie;
+        System.out.println("---------------------------------");
+        System.out.print("Enter movie name: ");
+        nameMovie = sc.nextLine().toLowerCase();
+
+        Double rateMovie;
+
+        while (true){
+            try {
+                System.out.println("---------------------------------");
+                System.out.print("Enter the new movie rating: ");
+                rateMovie = sc.nextDouble();
+
+                if (rateMovie >= 0 || rateMovie <= 5) {
+                    break;
+                } else {
+                    System.out.println("The rating needs to be beetween 0 and 5 stars");
+                }
+            }catch (InputMismatchException e){
+                System.out.println("You can only enter number.");
+            }
+        }
+        map.put(nameMovie, rateMovie);
+    }
+
+    static void retrieve(Scanner sc, Map<String, Double> map){
+        String nameMovie;
+        System.out.println("---------------------------------");
+        System.out.print("Enter movie name: ");
+        nameMovie = sc.nextLine();
+
+        if (map.containsKey(nameMovie)) {
+            System.out.printf("The movie %s has rating %.1f. \n", nameMovie, map.get(nameMovie));
+        }else{
+            System.out.printf("The movie %s has not in list. \n", nameMovie);
+        }
+
+    }
+
+    static void showList(Map<String, Double> map){
+        ArrayList<String> listNames = new ArrayList<>(map.keySet());
+        ArrayList<Double> listRating = new ArrayList<Double>(map.values());
+
+        System.out.println("---------------------------------");
+        System.out.println("List:");
+        for (int i = 0; i < listNames.size(); i++) {
+            System.out.printf("[%d] %s - %.1f stars \n", i, listNames.get(i), listRating.get(i));
+        }
+    }
+}
